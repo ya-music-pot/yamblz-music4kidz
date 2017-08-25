3398e1fa21732210dea3bf62464d055668e7d3d0
Elena I <ivanovaev90@gmail.com>
2017-08-25 18:57:16 +0300

    Поправлено название компонент внутри страниц.

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Player extends Component {
   render() {
     return (
       <div>
--- src/pages/Playlist/index.js
+++ src/pages/Playlist/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Playlist extends Component {
   render() {
     return (
       <div>
