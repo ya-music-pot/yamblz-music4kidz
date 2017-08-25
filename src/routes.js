e142f5f40fe44544ed4e355f5fc2c0750af17c05
Elena I <ivanovaev90@gmail.com>
2017-08-25 19:05:44 +0300

    Добавлены роутеры для новых страниц.

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,20 @@ import { Switch, Route } from 'react-router-dom';
 import App from '_pages/App';
 import Entrance from '_pages/Entrance';
 import SetUp from '_pages/SetUp';
+import Mood from '_pages/Mood';
+import Action from '_pages/Action';
+import Playlist from '_pages/Playlist';
+import Player from '_pages/Player';
 
 const routes = (
   <Switch>
     <App>
       <Route exact path="/" component={Entrance} />
       <Route path="/setup" component={SetUp} />
+      <Route path="/setup" component={Mood} />
+      <Route path="/setup" component={Action} />
+      <Route path="/setup" component={Playlist} />
+      <Route path="/setup" component={Player} />
     </App>
   </Switch>
 );
