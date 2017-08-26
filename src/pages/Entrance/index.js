7cc774650957bd2854f635d69e92668327db0fec
Maria Evstropova <m.evstropova@yandex.ru>
2017-08-26 18:31:02 +0300

    добавила всёрстку для начальной страницы, но для кнопки вёрстки нет

--- src/pages/Entrance/index.js
+++ src/pages/Entrance/index.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Example from '_components/Example';
+import style from './style.scss';
+import entranceCloud from './images/entrance-cloud.png';
+import entranceCloud2x from './images/entrance-cloud@2x.png';
+import entranceCloud3x from './images/entrance-cloud@3x.png';
 
 class Entrance extends Component {
-  _onClick = (e) => {
-    console.log(1111)
+  _handleClick = (e) => {
+    this.props.history.push('/setup');
   }
 
   render() {
     return (
-      <div>
-        Вход
-        <Example onClick={this._onClick} />
+      <div className={style.container}>
+        <div className={style.imageContainer}>
+          <img 
+            src={entranceCloud}
+            srcSet={`${entranceCloud} 1x, ${entranceCloud2x} 2x, ${entranceCloud3x} 3x`}
+          />
+        </div>
+        <div className={style.textContainer}>
+          <div className={style.title}>
+            Привет!
+          </div>
+          <div className={style.subTitle}>
+            Давай слушать музыку и веселиться? 
+          </div>
+        </div>
+        <div onClick={this._handleClick}>Кнопка</div>
+        <div className={style.cardContainer}>
+          Карточка
+        </div>
       </div>
     );
   }
