c70a0c430fa036d80b4cd9b42ef7961bba4a6369
Maria Evstropova <m.evstropova@yandex.ru>
2017-08-27 08:27:14 +0300

    добавила 2й экран, если ребенок не хочет подбирать плейлист

---
+++ src/pages/Entrance/screens/EntranceScreen.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from '_components/Button';
+
+import style from '../style.scss';
+import entranceCloud from '../images/entrance-cloud.png';
+import entranceCloud2x from '../images/entrance-cloud@2x.png';
+import entranceCloud3x from '../images/entrance-cloud@3x.png';
+
+export default class EntranceScreen extends Component {
+  render() {
+    return (
+      <div className={style.container}>
+        <div className={style.imageContainer}>
+          <img
+            alt="flying-cloud"
+            src={entranceCloud}
+            srcSet={`${entranceCloud} 1x, ${entranceCloud2x} 2x, ${entranceCloud3x} 3x`}
+          />
+        </div>
+        <div className={style.textContainer}>
+          <Title />
+          <Button
+            style={style.button}
+            label="Поехали"
+            onClick={this.props.onNavigate}
+          />
+        </div>
+        <div className={style.cardContainer}>
+          Карточка
+        </div>
+      </div>
+    );
+  }
+}
+
+EntranceScreen.propTypes = {
+  onNavigate: PropTypes.func.isRequired,
+};
+
+const Title = () => {
+  return (
+    <div>
+      <div className={style.title}>
+        Привет!
+      </div>
+      <div className={style.subTitle}>
+        Давай слушать музыку и веселиться?
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/Entrance/screens/PlaylistCalibration.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from '_components/Button';
+
+import style from '../style.scss';
+
+export default class PlaylistCalibration extends Component {
+  render() {
+    return (
+      <div className={style.container}>
+        <div className={style.titleContainer}>
+          <div className={style.title}>
+            А давай создадим лучший в мире плейлист для тебя?
+          </div>
+        </div>
+        <Button
+          style={`${style.button} ${style.buttonLower}`}
+          label="Создать"
+          onClick={this.props.onAccept}
+        />
+        <Button
+          style={style.buttonSmall}
+          label="Не хочу сейчас"
+          onClick={this.props.onDeny}
+        />
+      </div>
+    );
+  }
+}
+
+PlaylistCalibration.propTypes = {
+  onAccept: PropTypes.func.isRequired,
+  onDeny: PropTypes.func.isRequired,
+};
