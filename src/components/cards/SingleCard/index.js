c9b5dbdb8d5fb98f5b7f95f5eced51636ec1d6b1
Yulia Seredich <yseredich@gmail.com>
2017-09-07 13:37:34 +0300

    убрала багу с фонами

--- src/components/cards/SingleCard/index.js
+++ src/components/cards/SingleCard/index.js
@@ -8,6 +8,19 @@ import ButtonMiniplayer from '_components/ButtonMiniplayer';
 import style from './style.styl';
 
 export default class SingleCard extends Component {
+  constructor() {
+    super();
+    this._bg = null;
+  }
+
+  componentWillMount() {
+    if (this._bg === null) {
+      const { gradients } = this.props.bgs;
+      const gradient = gradients[getRandomInteger(0, gradients.length - 1)];
+      this._bg = { backgroundImage: `linear-gradient(${gradient})` };
+    }
+  }
+
   _handleCardClick = () => {
     const {
       data: { tracks },
@@ -43,22 +56,18 @@ export default class SingleCard extends Component {
       info, singer, button, overlay,
     } = style;
 
-    const { bgs, data } = this.props;
     const {
       artist, name, image_url: imageUrl,
-    } = data.tracks[0];
+    } = this.props.data.tracks[0];
 
     const imageStyles = {};
     if (imageUrl) {
       imageStyles.backgroundImage = `url(${imageUrl})`;
     }
 
-    const gradient = bgs.gradients[getRandomInteger(0, bgs.gradients.length - 1)];
-    const backgroundStyles = { backgroundImage: `linear-gradient(${gradient})` };
-
     return (
       <div className={container} style={imageStyles} onClick={this._handleCardClick}>
-        <div className={overlay} style={backgroundStyles} />
+        <div className={overlay} style={this._bg} />
         <div className={content}>
           <CardTitle text="Модный трек" styles={title} />
           <div className={info}>
