382da2c84ce25cb3692ce53b8c3cd22b54c5bbfc
Elena Ivanova <ivanovaev90@gmail.com>
2017-09-07 13:10:11 +0300

    Поправлена верстка второго экрана.

--- src/pages/Entrance/screens/PlaylistCalibration.js
+++ src/pages/Entrance/screens/PlaylistCalibration.js
@@ -11,7 +11,7 @@ export default class PlaylistCalibration extends Component {
       <div className={style.container}>
         <div className={style.calibrationTitleContainer}>
           <div className={style.title}>
-            А давай создадим лучший в мире плейлист для тебя?
+            А давай создадим<br />лучший в мире<br />плейлист для тебя?
           </div>
         </div>
         <div className={style.calibrationButtonWrapper}>
