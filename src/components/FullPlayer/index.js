a0cf356c4c1214d853714b145dba0f3dbdfe8ef7
Elena I <ivanovaev90@gmail.com>
2017-09-10 16:53:04 +0300

    Удалена лишняя прослойка Container.

--- src/components/FullPlayer/index.js
+++ src/components/FullPlayer/index.js
@@ -1,45 +1,189 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Container from '_components/FullPlayer/Container';
+import cl from 'classname';
+
+import Button from '_components/Button';
+import CircularAvatar from '_components/CircularAvatar';
+import Icon from '_components/Icon';
+
+import CARDS from '_data/cardsType';
+
+import Background from './Background';
+
+import style from './style.styl';
 
 export default class FullPlayer extends Component {
-  /**
-   * @function _handleButtonPressed - Служит для обработки кликов по кнопке стой \ пой
-   * Запускает и приостанавливает воспроизведение музыки
-   * */
+  _handleClickDislike = () => {
 
-  render() {
-    const playerState = this.props.playerState;
+  };
+
+  _handleClickLike = () => {
+
+  };
+
+  _handleClickPlus = () => {
+
+  };
+
+  renderHeader() {
+    const { headerRow, buttonArrowDown, moodIcons } = style;
+    // TODO: получать эмоции и класть name в стор
+    const { onClickArrowDown, cardType } = this.props;
+    const name = 'say my name';
+
+    return (
+      <div className={headerRow}>
+        <Button style={buttonArrowDown} onClick={onClickArrowDown} />
+        <div className={moodIcons}>{getTitleByCard(cardType, name)}</div>
+      </div>
+    );
+  }
+
+  renderLike() {
+    const { vote, buttonDislike, spacer, buttonLike } = style;
+    return (
+      <div className={vote}>
+        <Button style={buttonDislike} onClick={this._handleClickDislike} />
+        <div className={spacer} />
+        <Button style={buttonLike} onClick={this._handleClickLike} />
+      </div>
+    );
+  }
+
+  renderControls() {
+    const { controlsRow, buttonPrevious, buttonNext } = style;
     const {
       onTogglePlay, onClickNext, onClickPrevious,
-      onClickRepeat, onClickArrowDown, cardType,
-      openListTracks,
+      playerState: { isPlaying }, cardType,
     } = this.props;
+    let isPrevNeeded = true;
+    switch (cardType) {
+      case CARDS.radio:
+      case CARDS.single:
+      case CARDS.personal:
+        isPrevNeeded = false;
+        break;
+      default:
+        break;
+    }
+
+    return (
+      <div className={controlsRow}>
+        { isPrevNeeded && <Button style={buttonPrevious} onClick={onClickPrevious} /> }
+        <Button
+          style={
+            cl(
+              style.playerButton,
+              isPlaying ? style.playerButtonPause : style.playerButtonPlay,
+            )
+          }
+          isPlaying={isPlaying}
+          onClick={onTogglePlay}
+        />
+        { cardType !== CARDS.single && <Button style={buttonNext} onClick={onClickNext} /> }
+      </div>
+    );
+  }
+
+  renderFooter() {
+    const {
+      bottomRow, buttonPlus, buttonRepeatActive,
+      buttonRepeatInactive, buttonList,
+    } = style;
+    const {
+      onClickRepeat, cardType, openListTracks,
+      playerState: { isRepeatMode },
+    } = this.props;
+
+    let playlistControl = (<div>
+      <Button style={buttonList} onClick={openListTracks}>
+        <Icon className={style.icon} typeIcon="player-list" />
+      </Button>
+    </div>);
+    if (cardType === CARDS.radio || cardType === CARDS.single) {
+      playlistControl = null;
+    } else if (cardType === CARDS.personal) {
+      playlistControl = (<div>balloon</div>);
+    }
+
+    return (
+      <div className={bottomRow}>
+        <Button style={buttonPlus} onClick={this._handleClickPlus} />
+        { playlistControl }
+        <Button
+          style={isRepeatMode ? buttonRepeatActive : buttonRepeatInactive}
+          onClick={onClickRepeat}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    const {
+      playerState: {
+        trackName, singerName, position,
+        duration, cover,
+      }, cardType,
+    } = this.props;
+
+    const percentage = position / duration;
+    const diffTrackPosition = position - duration;
+    const minutesLeft = parseInt(diffTrackPosition / 60, 10).toString();
+    const sec = -(parseInt(diffTrackPosition, 10) - minutesLeft * 60);
+    const secondsLeft = (sec < 10 ? `0${sec}` : sec).toString();
 
     return (
       <div>
-        <Container
-          playerState={playerState}
-          onTogglePlay={onTogglePlay}
-          openListTracks={openListTracks}
-          onClickNext={onClickNext}
-          onClickPrevious={onClickPrevious}
-          onClickRepeat={onClickRepeat}
-          onClickArrowDown={onClickArrowDown}
-          cardType={cardType}
-        />
+        <div className={style.wrapper}>
+          { this.renderHeader() }
+          <div>
+            { cardType === CARDS.personal && this.renderLike() }
+            <CircularAvatar
+              image={cover}
+              progress={percentage}
+              radius={0.18}
+              time={`${minutesLeft}:${secondsLeft}`}
+            />
+          </div>
+          <div className={style.titleRow}>
+            <div className={style.songName}>{trackName}</div>
+            <div className={style.artistName}>{singerName}</div>
+          </div>
+          { this.renderControls() }
+          { this.renderFooter() }
+          <Background cover={cover} />
+        </div>
       </div>
     );
   }
 }
 
 FullPlayer.propTypes = {
-  playerState: PropTypes.object,
   onTogglePlay: PropTypes.func,
-  openListTracks: PropTypes.func,
   onClickPrevious: PropTypes.func,
   onClickNext: PropTypes.func,
   onClickRepeat: PropTypes.func,
   onClickArrowDown: PropTypes.func,
+  openListTracks: PropTypes.func,
+  playerState: PropTypes.object,
   cardType: PropTypes.number,
 };
+
+/*
+  Helpers
+ */
+function getTitleByCard(cardType, name) {
+  switch (cardType) {
+    case CARDS.radio:
+      return `Радио ${name}`;
+    case CARDS.single:
+      return 'Модный трек';
+    case CARDS.game:
+      return 'Слушай и играй';
+    case CARDS.personal:
+      return 'emoji';
+    default:
+      return null;
+  }
+}
+
