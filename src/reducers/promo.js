5ed9cd53eba20fa4f3abcf967c652154f476c2a7
Elena I <ivanovaev90@gmail.com>
2017-09-10 19:54:31 +0300

    Добавлены правельные данные для промо.

--- src/reducers/promo.js
+++ src/reducers/promo.js
@@ -1,30 +1,5 @@
-import { GET_PROMO } from '_actions/promo';
 import data from '_data/promo';
 
-export default function (state = { data }, action) {
-  const { type, response } = action;
-
-  switch (type) {
-    case `${GET_PROMO}_START`:
-      return {
-        ...state,
-        loading: true,
-        loaded: false,
-      };
-    case `${GET_PROMO}_FAIL`:
-      return {
-        ...state,
-        loading: false,
-        loaded: true,
-      };
-    case `${GET_PROMO}_SUCCESS`:
-      return {
-        ...state,
-        loading: false,
-        loaded: true,
-        ...response,
-      };
-    default:
-      return state;
-  }
+export default function (state = { data }) {
+  return state;
 }
