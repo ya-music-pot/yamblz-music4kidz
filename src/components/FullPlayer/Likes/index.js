036f8b0e1203bdbcb6a146b1e7686c31d25d4029
Yulia Seredich <yseredich@gmail.com>
2017-09-10 22:01:09 +0300

    переделала вызов колбеков

--- src/components/FullPlayer/Likes/index.js
+++ src/components/FullPlayer/Likes/index.js
@@ -5,24 +5,18 @@ import Button from '_components/Button';
 import style from '../style.styl';
 
 export default class Likes extends Component {
-  _handleClickDislike = () => {
-    this.props.onDislikeClick();
-  };
-
-  _handleClickLike = () => {
-    this.props.onLikeClick();
-  };
-
   render() {
     const {
       vote, buttonDislike, spacer,
       buttonLike,
     } = style;
+
+    const { onLikeClick, onDislikeClick } = this.props;
     return (
       <div className={vote}>
-        <Button style={buttonDislike} onClick={this._handleClickDislike} />
+        <Button style={buttonDislike} onClick={onDislikeClick} />
         <div className={spacer} />
-        <Button style={buttonLike} onClick={this._handleClickLike} />
+        <Button style={buttonLike} onClick={onLikeClick} />
       </div>
     );
   }
