7d633222e241b2469cbbadadbe0f34b03ebc0063
Maria Evstropova <m.evstropova@yandex.ru>
2017-09-12 20:39:31 +0300

    новые ачивки и поправлен цвет текста

--- src/data/achievements.js
+++ src/data/achievements.js
@@ -1,50 +1,35 @@
 export default {
-  order: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  order: [1, 2, 3, 4, 5, 6],
   data: {
     1: {
       id: 1,
-      title: 'Вкусные новинки',
-      typeIcon: 'achievement-new',
+      title: 'На волне хайпа',
+      typeIcon: 'achievement-hype',
     },
     2: {
       id: 2,
-      title: 'Королева поп-музыки',
-      typeIcon: 'achievement-pop',
+      title: 'Вкусные новинки',
+      typeIcon: 'achievement-new',
     },
     3: {
       id: 3,
-      title: 'Вкусные новинки',
-      typeIcon: 'achievement-new',
+      title: 'Королева поп-музыки',
+      typeIcon: 'achievement-pop',
     },
     4: {
       id: 4,
-      title: 'Королева поп-музыки',
-      typeIcon: 'achievement-pop',
+      title: 'Дружба на века',
+      typeIcon: 'achievement-friend',
     },
     5: {
       id: 5,
-      title: 'Вкусные новинки',
-      typeIcon: 'achievement-new',
+      title: 'Круче всех!',
+      typeIcon: 'achievement-superb',
     },
     6: {
       id: 6,
-      title: 'Королева поп-музыки',
-      typeIcon: 'achievement-pop',
-    },
-    7: {
-      id: 7,
-      title: 'Вкусные новинки',
-      typeIcon: 'achievement-new',
-    },
-    8: {
-      id: 8,
-      title: 'Королева поп-музыки',
-      typeIcon: 'achievement-pop',
-    },
-    9: {
-      id: 9,
-      title: 'Вкусные новинки',
-      typeIcon: 'achievement-new',
+      title: 'Улётный вкус',
+      typeIcon: 'achievement-taste',
     },
   },
 };
