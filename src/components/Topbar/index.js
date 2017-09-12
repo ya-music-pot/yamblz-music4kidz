d37db768e8cc46f1da8420ced469ff957ef622f7
Maria Evstropova <m.evstropova@yandex.ru>
2017-09-12 21:13:23 +0300

    переход на профиль, не показывать иконку, если пользователь не прошел онбординг

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import cl from 'classname';
 import Avatar from '_components/Avatar';
 
 import style from './style.styl';
 
 export default class Topbar extends Component {
   render() {
+    const { onClick, user } = this.props;
+
     return (
       <div className={style.topbar}>
-        <Avatar />
+        {user.id &&
+          <Avatar
+            onClick={onClick}
+            avatar={user.avatarUrl}
+            className={cl(user.avatarUrl && style.avatarCover)}
+          />}
       </div>
     );
   }
 }
 
-Topbar.propTypes = {};
+Topbar.propTypes = {
+  onClick: PropTypes.func,
+  user: PropTypes.object,
+};
+
